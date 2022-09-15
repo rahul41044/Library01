ce3daa3f7eaf3fdc35cb5696dc36c60c141ffccd
Rahul <rahul41044@gmail.com>
2022-09-15 15:27:26 +0530

    comment added

--- libmanage/admin.py
+++ libmanage/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from.models import Student
 
 # Register your models here.
+# comment
 @admin.register(Student)
 class StudentModelAdmin(admin.ModelAdmin):
     list_display=['id','book','author','issuedto','issueddate']
